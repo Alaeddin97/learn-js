f0e94c91c8cdfcbba57290c5894406bd90bf238d
Ala <ala>
2022-12-18 14:22:38 +0100

    05-Guess-My-Number

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+let guess = Math.trunc(Math.random() * 20)+1;
+let score = +document.getElementById('score').innerText;
+let highScore = +document.getElementById('highscore').innerText;
+
+
+function onCheck() {
+  const x = document.getElementById('guess').value;
+  if (+x < guess) {
+    document.getElementById('message').innerText = 'Too low';
+    --score;
+    document.getElementById('score').innerText = score;
+  } else if (+x > guess) {
+    document.getElementById('message').innerText = 'Too high';
+    --score;
+    document.getElementById('score').innerText = score;
+  } else {
+    document.getElementById('message').innerText = 'Yohoo! good guess !💪✌👌';
+    highScore += score;
+    document.getElementById('highscore').innerText = highScore;
+    document.getElementById('number').innerText=highScore;
+    document.body.style.backgroundColor='#60b347';
+  }
+}
+
+function onAgain(){
+    document.body.style.backgroundColor='#222';
+    document.getElementById('message').innerText = 'Start guessing...';
+    document.getElementById('number').innerText='?';
+    score = 20;
+    document.getElementById('score').innerText = score;
+    guess = Math.trunc(Math.random() * 20);
+}
