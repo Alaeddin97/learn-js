425964cf35235ed12cfb8e173851e0d89ad1c56f
Alaeddin97 <alagarraouiala@gmail.com>
2023-01-11 11:44:53 +0100

    more data structure

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,29 +1,47 @@
 'use strict';
+
+const openingHours={
+  //ES6 : can compute the key not only the value:
+  [`day-${2+2}`]: {
+    open: 12,
+    close: 22,
+  },
+  mon: {
+    open: 11,
+    close: 23,
+  },
+  wed: {
+    open: 13,
+    close: 23,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   localization: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  orderPasta: function (ing1, ing2, ing3) {
+  //ES6 : enhanced literal objects
+  openingHours,
+  // orderPasta: function (ing1, ing2, ing3) {
+  //   console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and 
+  //   ${ing3}`);
+  // },
+  // ES6 enhanced literal objects
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and 
     ${ing3}`);
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
@@ -177,7 +195,7 @@ console.log(guestsToo);
 
 
 
-
+/*
 // Logical assignment operators
 const rest1={
   name:'Burguer',
@@ -196,14 +214,89 @@ rest1.guests ||=5;
 console.log(rest1.guests);
 rest2.numGuests||=10;
 console.log(rest2.numGuests);
-*/
+
 // AND assignment operators
 // works with nullish values
 rest1.name ??='Burguer King';
 console.log(rest1.name);
 rest1.guests ??=5;
 console.log(rest1.guests);
+*/
 
 
 
+
+
+// for... of
+/*
+const menu=[...restaurant.mainMenu,...restaurant.starterMenu];
+for(const item of menu)console.log(item);
+for(const item of menu.entries())console.log(item);
+for(const [i,meal] of menu.entries()){
+  console.log(`${i+1}: ${meal}`);
+};
+*/
+
+
+
+
+// Optional chaining
+// console.log(openingHours.mon.open);// erreur : read from undefined object
+
+// With optional chaining
+console.log(openingHours?.mon?.open);
+
+const days=['mon','tue','wed','thu','fri','sat','sun'];
+
+for(const day of days){
+  const open=openingHours[day]?.open ?? 'closed'; // nullish value with ?? and ?. as well
+  console.log(`On ${day} we open at ${open}`)
+}
+
+
+
+
+
+
+
+
+
+/*
+// Challenge #1: football game:
+
+//1: 
+const team1='FC Barça';
+const team2='Real Madrid';
+const player1=['Stegen ','Christensen', 'Araujo','Garcia','Balde','De Jong', 'Gavi','Pedri','Dembele', 'Lewandowski','Ferran'];
+const player2=['Courtois','Carvajal','Militao','Alaba','Mendy ','Modric','Tchouaméni','Kroos ','Valverde','Benzema','Vinicius'];
+
+//2:
+const gk=player1[0];
+const [,...fieldPlayers]=player1;
+console.log(fieldPlayers);
+
+// 3:
+const allPlayers=[...player1,...player2];
+
+//4:
+const players1Final=[...player1,'Alba','Fati','Busquets'];
+
+//5:
+const odds={team1,team2,x:'draw'};
+odds.team1=3;
+odds.team2=5;
+console.log(odds);
+
+// 6:
+function printGoals(...names){
+  console.log([...names]);
+  console.log([...names].length + ' goals has been scored');
+}
+printGoals(player1[10],player1[10],player1[9]);
+
+//7:
+odds.team1<odds.team2 && console.log(`${team1} is more likely to win`);
+odds.team1>odds.team2 && console.log(`${team2} is more likely to win`);
+
+*/
 
